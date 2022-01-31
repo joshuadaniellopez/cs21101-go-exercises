cf5f620231b441b7d5552c13538b472ac0d8e516
Joshua Daniel Lopez <jdlopez.dev@gmail.com>
2022-01-31 16:18:08 +0800

    chore: initial models for the project

---
+++ project/server/models.go
@@ -0,0 +1,24 @@
+package server
+
+type User struct {
+	Id       int    `json:"id" bson:"id"`
+	Username string `json:"username" bson:"username"`
+	Name     string `json:"name" bson:"name"`
+}
+
+type Account struct {
+	Id   int    `json:"id" bson:"id"`
+	Name string `json:"name" bson:"name"`
+}
+
+type Bucket struct {
+	Id   int    `json:"id" bson:"id"`
+	Name string `json:"name" bson:"name"`
+}
+
+type LineItem struct {
+	Id          int     `json:"id" bson:"id"`
+	Title       string  `json:"title" bson:"title"`
+	Description string  `json:"description" bson:"description"`
+	Amount      float64 `json:"amount" bson:"amount"`
+}
